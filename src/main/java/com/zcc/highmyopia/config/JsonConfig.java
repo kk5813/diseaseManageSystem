a01bd79533c1e8807941f8f8176189e4e1bfe1ee
kk <2287996531@qq.com>
2025-01-02 17:46:31 +0800

    fix:注释掉JsonConfig

--- src/main/java/com/zcc/highmyopia/config/JsonConfig.java
+++ src/main/java/com/zcc/highmyopia/config/JsonConfig.java
@@ -1,32 +1,32 @@
-package com.zcc.highmyopia.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import org.springframework.boot.jackson.JsonComponent;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-/**
- * @ClassName JsonConfig
- * @Description
- * @Author aigao
- * @Date 2024/12/31 17:38
- * @Version 1.0
- */
-@JsonComponent
-public class JsonConfig {
-
-    /**
-     * 添加 Long 转 json 精度丢失的配置
-     */
-    @Bean
-    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
-        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(Long.class, ToStringSerializer.instance);
-        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
-        objectMapper.registerModule(module);
-        return objectMapper;
-    }
-}
+//package com.zcc.highmyopia.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.module.SimpleModule;
+//import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+//import org.springframework.boot.jackson.JsonComponent;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+//
+///**
+// * @ClassName JsonConfig
+// * @Description
+// * @Author aigao
+// * @Date 2024/12/31 17:38
+// * @Version 1.0
+// */
+//@JsonComponent
+//public class JsonConfig {
+//
+//    /**
+//     * 添加 Long 转 json 精度丢失的配置
+//     */
+//    @Bean
+//    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+//        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+//        SimpleModule module = new SimpleModule();
+//        module.addSerializer(Long.class, ToStringSerializer.instance);
+//        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+//        objectMapper.registerModule(module);
+//        return objectMapper;
+//    }
+//}
